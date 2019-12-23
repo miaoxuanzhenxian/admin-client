ae028f09e86c67f622b048ef79a70a0662d3d787
lmy <1751258687@qq.com>
2019-12-24 04:17:30 +0800

    内存中存储user信息，实现只读取localStorage对应的文件数据一次，剩下的其它的都从内存中读取，提高运行效率

--- src/utils/memoryUtils.js
+++ src/utils/memoryUtils.js
@@ -2,9 +2,9 @@ import { getUser } from './storageUtils';
 
 export default {
   /*
-    用来内存中存储登陆用户的信息,实现只读取localStorage文件数据一次，剩下的其它的都从内存中读取，
-    提高运行效率,初始值为local中读取的user;
-    也可以用全局状态管理（如redux，mobx等）实现内存中存储，实现只读取localStorage文件数据一次，
+    用来内存中存储登陆用户的信息,实现只读取localStorage对应的文件数据一次，剩下的其它的都从内存中读取，
+    提高运行效率,初始值为localStorage中读取的user;
+    也可以用全局状态管理（如redux，mobx等）实现内存中存储，实现只读取localStorage对应的文件数据一次，
     剩下的其它的都从内存中读取，提高运行效率;
     具体用哪种方式看情况，灵活变通，以最短的时间不择手段的最快完成全部基本功能为最先原则，其它都往后放。
   */
