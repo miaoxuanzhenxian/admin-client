7ddad992e4b28c84581a69d177c0b444f0e1b58d
lmy <1751258687@qq.com>
2019-12-24 21:13:31 +0800

    初步完成Admin组件的整体结构

---
+++ src/components/left-nav/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import style from './index.module.less';
+
+export default class LeftNav extends Component {
+  render() {
+    return (
+      <div className={style["left-nav"]}>
+        LeftNav
+      </div>
+    )
+  }
+}
--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import { Layout } from 'antd';
 
 // import { getUser } from '@/utils/storageUtils.js';
 import memoryUtils from '@/utils/memoryUtils';
+import LeftNav from '@/components/left-nav';
+import Header from '@/components/header';
+
+
+const { Footer, Sider, Content } = Layout;
 
 export default class Admin extends Component {
 
@@ -14,9 +20,18 @@ export default class Admin extends Component {
       return <Redirect to='/login' />
     }
     return (
-      <div>
-        hello, {user.username}
-      </div>
+      <Layout style={{ height: '100%' }}>
+        <Sider>
+          <LeftNav />
+        </Sider>
+        <Layout>
+          <Header />
+          <Content style={{ backgroundColor: 'white' }}>Content</Content>
+          <Footer style={{ textAlign: 'center', color: 'rgba(0, 0, 0, 0.5)' }}>
+            推荐使用谷歌浏览器，可以获得更佳页面操作体验
+          </Footer>
+        </Layout>
+      </Layout>
     )
   }
 }
