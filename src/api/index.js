57e5c67ae8810a538dac600917046f34f0e709d0
lmy <1751258687@qq.com>
2020-01-07 00:34:33 +0800

    Category组件实现动态显示分类列表

--- src/api/index.js
+++ src/api/index.js
@@ -4,10 +4,11 @@
 import ajax from './ajax';
 import jsonp from 'jsonp';
 import { message } from 'antd';
-const baseURL= 'http://localhost:5000';
+
+const BASE = 'http://localhost:5000';
 
 //请求登录
-export const reqLogin = (username, password) => ajax.post(baseURL + '/login', {username, password});
+export const reqLogin = (username, password) => ajax.post(BASE + '/login', { username, password });
 /* export function reqLogin (username, password) {
   return ajax({
     method: 'post',
@@ -23,7 +24,7 @@ export const reqLogin = (username, password) => ajax.post(baseURL + '/login', {u
 export const reqWeather = (city) => {
   return new Promise((resolve, reject) => {// 执行器函数: 内部去执行异步任务, 成功了调用resolve(), 失败了不调用reject(), 直接提示错误
     const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`;
-    jsonp(url, {timeout: 7000}, (err, data) => {
+    jsonp(url, { timeout: 7000 }, (err, data) => {
       if (!err && data.error === 0) {
         const { dayPictureUrl, weather } = data.results[0].weather_data[0]
         resolve({ dayPictureUrl, weather });
@@ -33,3 +34,6 @@ export const reqWeather = (city) => {
     });
   });
 }
+
+//获取分类列表
+export const reqCategorys = () => ajax(BASE + '/manage/category/list');
--- src/pages/category/index.js
+++ src/pages/category/index.js
@@ -1,61 +1,75 @@
 import React, { Component } from 'react';
-import { Card, Button, Icon, Table } from 'antd';
+import { Card, Button, Icon, Table, message } from 'antd';
 import LinkButton from '@/components/link-button';
 
 import style from './index.module.less';
-
-const columns = [
-  {
-    title: '分类名称',
-    dataIndex: 'name',
-  },
-  {
-    title: '操作',
-    width: '300px',
-    render: () => <LinkButton>修改分类</LinkButton>
-  },
-];
-
-const data = [
-  {
-    "_id": "5e11d7b560f14026000f01f1",
-    "name": "aa",
-    "__v": 0
-  },
-  {
-    "_id": "5e11d7bb60f14026000f01f2",
-    "name": "bb",
-    "__v": 0
-  },
-  {
-    "_id": "5e11d7be60f14026000f01f3",
-    "name": "cc",
-    "__v": 0
-  },
-  {
-    "_id": "5e11d7c260f14026000f01f4",
-    "name": "dd",
-    "__v": 0
-  },
-  {
-    "_id": "5e11d7c660f14026000f01f5",
-    "name": "ee",
-    "__v": 0
-  },
-  {
-    "_id": "5e11d7cc60f14026000f01f6",
-    "name": "ff",
-    "__v": 0
-  },
-  {
-    "_id": "5e11d7d860f14026000f01f7",
-    "name": "gg",
-    "__v": 0
-  },
-];
+import { reqCategorys } from '@/api';
 
 export default class Category extends Component {
+  constructor(props) {
+    super(props)
+
+    this.initColumns();
+
+    this.state = {
+      categorys: [],
+      loading: false //是否正在请求加载中
+    }
+  }
+
+  componentDidMount() {
+    this.getCategorys();
+  }
+
+  componentWillUnmount() {
+    //不能对未安装的(或者已经卸载的）组件执行响应状态更新（setState）,以免造成内存泄漏
+    //即组件在卸载之后(比如你切换路由卸载一些组件),过期的页面中请求还没完成，而请求完成后却依然去更新了状态state（这里可能是setState），但此时此组件已被卸载了，不存在了。因此会造成内存泄漏，会报警告错误。
+    //因此要在卸载组件时(在componentWillUnmount()中)，取消异步任务setState()，即将setState()方法设为空函数。这样就不会警告报错了。
+    this.setState = () => {}
+  }
+
+  /*
+    初始化Table的所有列信息的数组
+  */
+  initColumns = () => {
+    this.columns = [
+      {
+        title: '分类名称',
+        dataIndex: 'name',
+      },
+      {
+        title: '操作',
+        width: '300px',
+        render: () => <LinkButton>修改分类</LinkButton>
+      },
+    ];
+  }
+
+  /*
+    异步获取分类列表显示
+  */
+  getCategorys = async () => {
+    //显示loading
+    this.setState({ loading: true });
+    //发异步ajax请求
+    const result = await reqCategorys();
+    if (result.status === 0) { //成功了
+      //取出分类列表
+      const categorys = result.data;
+      //更新状态categorys数据
+      this.setState({
+        categorys,
+        loading: false //成功了后隐藏loading
+      });
+    } else {
+      message.error('获取分类列表失败');
+      this.setState({ loading: false }); //获取分类列表失败后隐藏loading
+    }
+  }
+
   render() {
+    //取出状态数据
+    const { categorys, loading } = this.state;
     //Card右上角的结构
     const extra = (
       <Button type="primary">
@@ -67,11 +81,12 @@ export default class Category extends Component {
       <div>
         <Card extra={extra}>
           <Table
-            columns={columns}
-            dataSource={data}
+            loading={loading}
+            columns={this.columns}
+            dataSource={categorys}
             rowKey="_id"
             bordered
-            pagination={{ defaultPageSize: 3, showQuickJumper: true }}
+            pagination={{ defaultPageSize: 6, showQuickJumper: true }}
           />
         </Card>
       </div>
