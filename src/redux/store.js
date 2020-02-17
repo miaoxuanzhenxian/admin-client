f411114cea45086cdb061e31c8b0a87f2f09c173
lmy <1751258687@qq.com>
2020-02-17 23:28:35 +0800

    counter应用的react-redux的最终版本,利用redux-thunk实现redux异步，给浏览器安装上redux开发调式工具(redux DevTools)后，再安装redux-devtools-extension库，按需导入其中的composeWithDevTools函数，在createStore()的第二个参数中调用执行composeWithDevTools()函数包裹(传入)applyMiddleware(thunk)即可,实现能使用redux调试工具，能在redux调试工具(redux DevTools)中看到redux中的公共状态数据，方便我们调试

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,8 @@
   redux最核心的管理对象: store
 */
 import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk' // 注意:redux本身不支持异步，即dispatch()函数只接收同步action对象，因此利用redux-thunk库可以实现redux异步，即使得dispatch()可以接收函数也可以接收对象，此时同步操作时，给dispatch()函数传入同步action对象，异步操作时，给dispatch()函数传入异步action函数，在此函数体中，执行完异步操作后，再dispatch(同步action对象)分发一个同步action对象
+import { composeWithDevTools } from 'redux-devtools-extension' // 注意:此时放到redux中的多个组件共享的公共状态数据已经不在组件中了，而是放到redux中了，因此此时在react的调试工具(React Developer Tools)中是看不到放到redux中的公共状态数据的,因此此时要给浏览器安装redux开发调式工具(redux DevTools)，但给浏览器安装上redux开发调式工具(redux DevTools)后，还是不能使用redux调试工具，还是不能看到redux中的公共状态数据，还要安装redux-devtools-extension库，按需导入其中的composeWithDevTools函数，在createStore()的第二个参数中调用执行composeWithDevTools()函数包裹(传入)applyMiddleware(thunk)即可，此时，才能使用redux调试工具，才能在redux调试工具(redux DevTools)中看到redux中的公共状态数据，方便我们调试。
 
 import reducer from './reducer'
 
