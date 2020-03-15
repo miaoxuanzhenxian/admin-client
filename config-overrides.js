da2d2cb90fc34dbe9d892936b7bc987b69b5fcf5
lmy <1751258687@qq.com>
2020-03-15 20:33:49 +0800

    修改格式

--- config-overrides.js
+++ config-overrides.js
@@ -5,25 +5,25 @@ const path = require('path')
 process.env.GENERATE_SOURCEMAP = "false"
 
 module.exports = override(
-  //配置根路径别名
+  // 配置根路径别名
   addWebpackAlias({
-    '@': path.resolve(__dirname, 'src'),
+    '@': path.resolve(__dirname, 'src')
   }),
-  //配置装饰器
+  // 配置装饰器
   addDecoratorsLegacy(),
-  //按需加载antd
+  // 按需加载antd
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
-    style: true,
+    style: true
   }),
-   //配置使用less，及改变antd的主题
+   // 配置使用less，及改变antd的主题
    addLessLoader({
     javascriptEnabled: true,
-    modifyVars: { '@primary-color': '#1DA57A' },
+    modifyVars: { '@primary-color': '#1DA57A' }
   }),
-  //addWebpackModuleRule将提供的规则添加到webpack配置的module.rules数组中。链接https://github.com/arackaf/customize-cra/blob/master/api.md
-  //配置less module的less样式模块化编程，这样类似于vue的scoped，不污染命名空间，命名不会冲突。
+  // addWebpackModuleRule将提供的规则添加到webpack配置的module.rules数组中。链接https://github.com/arackaf/customize-cra/blob/master/api.md
+  // 配置less module的less样式模块化编程，这样类似于vue的scoped，不污染命名空间，命名不会冲突。
   addWebpackModuleRule({
     test: /\.module\.less$/,
     use: [
@@ -36,11 +36,11 @@ module.exports = override(
           // 1 => postcss-loader;
           // 2 => postcss-loader, sass-loader
           modules: {
-            localIdentName: '[folder]_[local]__[hash:base64:5]',
-          },
-        },
+            localIdentName: '[folder]_[local]__[hash:base64:5]'
+          }
+        }
       },
-      'less-loader',
-    ],
-  }),
+      'less-loader'
+    ]
+  })
 )
